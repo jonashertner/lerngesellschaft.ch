e23f02f47e960674d9de2f9669fff56eebd62883
Jonas Hertner <jonashertner@jonass-macbook-pro.home>
2026-09-16 08:43:32 +0200

    Unify Lernbus enquiries and clarify semester enrolment

--- src/_data/lernbusContact.js
+++ src/_data/lernbusContact.js
@@ -1,6 +1,6 @@
 // Public form endpoint; the recipient is configured and verified in Formspree.
 export default {
   endpoint: 'https://formspree.io/f/maeyrgjk',
-  recipient: 'jonashertner@lernbus.ch',
+  recipient: 'info@lernbus.ch',
   provider: 'Formspree'
 };
